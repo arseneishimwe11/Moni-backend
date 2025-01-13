eb484a737aab1e96f15d65ce22403ad561e82e5d
Arsene <arsene@kali>
2025-01-13 16:27:47 +0200

    notification service updated

--- apps/notification-service/src/app/notification.module.ts
+++ apps/notification-service/src/app/notification.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BullModule } from '@nestjs/bull';
 import { NotificationController } from './notification.controller';
@@ -17,6 +17,20 @@ import configs from 'config/config';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configs],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Notification, NotificationTemplate],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([Notification, NotificationTemplate]),
     RedisModule,
